10d15b6eb3d52f8dd0b4cb968c28b6a48f7d7db4
PIvanov <xeolit@gmail.com>
2024-06-17 22:24:22 +0300

    Добавлен PasswordGenerator.

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# passwordGenerator
+# randomPackage
+
+## Requirements
+* PHP >= 7.0
+
+## Installation
+
+You can add this library as a local, per-project dependency to your project using [Composer](https://getcomposer.org/):
+
+    composer require pyivanov/passwordGenerator
+
+If you only need this library during development, for instance to run your project's test suite, then you should add it as a development-time dependency:
+
+    composer require --dev pyivanov/passwordGenerator
+
+## Usage
+
+```php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$randomGenerator = new \PYIvanov\PasswordPackage\PasswordGenerator();
+
+$length = 8;
+$password = $randomGenerator->generate($length);
+
+echo ("Your password is: $password");
+
+```
---
+++ src/PasswordGenerator.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pyivanov\PasswordGenerator;
+
+use Random\RandomException;
+
+class PasswordGenerator
+{
+    /**
+     * A PHP function that will generate a secure random password.
+     *
+     * @param int $length The length that you want your random password to be.
+     * @return string The random password.
+     * @throws RandomException
+     */
+    public function generate(int $length): string
+    {
+        //A list of characters that can be used in our
+        //random password.
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!-.[]?*()';
+
+        //Create a blank string.
+        $password = '';
+
+        //Get the index of the last character in our $characters string.
+        $characterListLength = mb_strlen($characters, '8bit') - 1;
+
+        //Loop from 1 to the $length that was specified.
+        foreach (range(1, $length) as $i) {
+            $password .= $characters[random_int(0, $characterListLength)];
+        }
+
+        return $password;
+    }
+}
